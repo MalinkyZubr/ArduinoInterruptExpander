01206ec5048c984125dba7b6e2801a0dc4d62313
MalinkyZubr <ahumanbeing189@gmail.com>
2024-07-25 15:24:26 -0700

    finished quine mcluskey

--- hardware/logic/scripts/QuineMcluskey.py
+++ hardware/logic/scripts/QuineMcluskey.py
@@ -48,9 +48,11 @@ class Minterm:
         new_expression = {column:(value if column not in difference else "x") for column, value in self.expression.items()}
 
         new_minterm: Minterm = Minterm(new_expression, (self, minterm))
+        self.matched = True
+        minterm.set_matched()
 
         return new_minterm
-
+    
     def set_matched(self):
         self.matched = True
 
@@ -66,6 +68,9 @@ class Minterm:
             root_comprising = self.comprising[0].get_root_comprising() + self.comprising[1].get_root_comprising()
 
         return root_comprising
+    
+    def __eq__(self, other: Minterm):
+        return self.expression == other.expression
     
     def __str__(self):
         stringified: str = ""
@@ -113,7 +118,7 @@ def identify_minterms(output_group: list[dict[str, str]], output_column_header: 
         row.pop(output_column_header)
         minterm_object_list.append(Minterm(row, None, str(index)))
 
-    return minterms
+    return minterm_object_list
 
 def generate_groups(minterms: list[Minterm]) -> dict[int, list[Minterm]]:
     groups: dict[int, list[Minterm]] = dict()
@@ -147,6 +152,9 @@ def generate_matched_pairs_2_group(group_1: list[Minterm], group_2: list[Minterm
     
     return new_minterms, prime_implicants
 
+def get_unmatched_in_group(group: list[Minterm]) -> list[Minterm]:
+    return [minterm for minterm in group if not minterm.is_matched()]
+
 def generate_matched_pairs_group(groups: dict[int, list[Minterm]]) -> tuple[list[Minterm], list[Minterm]]:
     minterm_pairs: list[Minterm] = []
     prime_implicants: list[Minterm] = []
@@ -156,46 +164,70 @@ def generate_matched_pairs_group(groups: dict[int, list[Minterm]]) -> tuple[list
             generated_minterms, generated_prime_implicants = generate_matched_pairs_2_group(group, groups[group_num + 1])
             minterm_pairs += generated_minterms
             prime_implicants += generated_prime_implicants
+        else:
+            prime_implicants += get_unmatched_in_group(group)
     
     return minterm_pairs, prime_implicants
 
-def recurse_all_prime_implicants(minterms: list[Minterm]) -> list[Minterm]:
+def remove_duplicate_implicants(minterms: list[Minterm]) -> list[Minterm]:
+    current_index: int = 0
+
+    while current_index < len(minterms):
+        current_minterm: Minterm = minterms[current_index]
+
+        if minterms.count(current_minterm) > 1:
+            minterms.pop(current_index)
+        else:
+            current_index += 1
+    
+    return minterms
+
+def recurse_find_all_prime_implicants(minterms: list[Minterm]) -> list[Minterm]:
     groups: dict[int, list[Minterm]] = generate_groups(minterms)
     minterm_pairs, prime_implicants = generate_matched_pairs_group(groups)
 
-    if len(prime_implicants) < len(minterm_pairs):
-        prime_implicants += recurse_all_prime_implicants(minterm_pairs)
+    if len(minterm_pairs):
+        prime_implicants += recurse_find_all_prime_implicants(minterm_pairs)
 
-    return prime_implicants
+    return remove_duplicate_implicants(prime_implicants)
 
-def generate_prime_implicant_table(prime_implicants: list[Minterm]) -> dict[Minterm, list[Minterm]]:
-    origin_minterms_table: dict[Minterm, list[Minterm]]
+def generate_prime_implicant_table(prime_implicants: list[Minterm]) -> list[tuple[Minterm, list[Minterm]]]:
+    origin_minterms_table: list[tuple[Minterm, list[Minterm]]] = list()
 
     for prime_implicant in prime_implicants:
-        origin_minterms_table[prime_implicant] = prime_implicant.get_root_comprising()
+        origin_minterms_table.append((prime_implicant, prime_implicant.get_root_comprising()))
 
     return origin_minterms_table
 
-def identify_essential_prime_implicants(prime_implicants: list[Minterm], all_origin_minterms: list[Minterm]) -> set[Minterm]:
-    origin_minterms_table = generate_prime_implicant_table(prime_implicants)
-    essential_prime_implicants: set[Minterm] = set()
+def flatten_origin_minterms(prime_implicants_table: list[tuple[Minterm, list[Minterm]]]) -> list[Minterm]:
+    all_origin_minterms: list[Minterm] = []
 
-    for prime_implicant, associated_minterms in origin_minterms_table:
+    for prime_implicant, associated_minterms in prime_implicants_table:
+        all_origin_minterms += associated_minterms
+
+    return all_origin_minterms
+
+def identify_essential_prime_implicants(prime_implicants: list[Minterm]) -> set[Minterm]:
+    prime_implicants_table: list[tuple[Minterm, list[Minterm]]] = generate_prime_implicant_table(prime_implicants)
+    essential_prime_implicants: list[Minterm] = list()
+    all_origin_minterms: list[Minterm] = flatten_origin_minterms(prime_implicants_table)
+
+    for prime_implicant, associated_minterms in prime_implicants_table:
         for minterm in associated_minterms:
-            if all_origin_minterms.count(minterm) == 1:
-                essential_prime_implicants.add(prime_implicant)
+            if all_origin_minterms.count(minterm) == 1 and minterm not in essential_prime_implicants:
+                essential_prime_implicants.append(prime_implicant)
     
     return essential_prime_implicants
 
 def format_expression(essential_prime_implicants: list[Minterm], output_column_header: str) -> str:
-    return output_column_header + ": " + " + ".join([str(epi) for epi in essential_prime_implicants])
+    return output_column_header + ": " + " + ".join({str(epi) for epi in essential_prime_implicants})
 
 def quine_mckluskey_minimize(boolean_group: list[dict[str, str]], output_column_header: str) -> str:
     all_origin_minterms: list[Minterm] = identify_minterms(boolean_group, output_column_header)
-    prime_implicants: list[Minterm] = recurse_all_prime_implicants(all_origin_minterms)
-    essential_prime_implicants: list[Minterm] = identify_essential_prime_implicants(prime_implicants, all_origin_minterms)
+    prime_implicants: list[Minterm] = recurse_find_all_prime_implicants(all_origin_minterms)
+    essential_prime_implicants: list[Minterm] = identify_essential_prime_implicants(prime_implicants)
 
-    return format_expression(essential_prime_implicants)
+    return format_expression(essential_prime_implicants, output_column_header)
 
 def quine_mckluskey_mppool(output_group: tuple[list[dict[str, str]], str]):
     return quine_mckluskey_minimize(output_group[1], output_group[0])
@@ -203,10 +235,24 @@ def quine_mckluskey_mppool(output_group: tuple[list[dict[str, str]], str]):
 def minimize_dataset(dataset_loaded: list[dict[str, str]]) -> list[str]:
     separated_output_tables: dict[str, dict[str, str]] = separate_outputs(dataset_loaded)
     
-    with multiprocessing.Pool(len(separated_output_tables)) as p:
-        fully_processed = p.map(quine_mckluskey_mppool, list(separated_output_tables.items()))
+    # with multiprocessing.Pool(len(separated_output_tables)) as p:
+    #     fully_processed = p.map(quine_mckluskey_mppool, list(separated_output_tables.items()))
+    
+    fully_processed = [quine_mckluskey_minimize(group, header) for header, group in separated_output_tables.items()]
 
     return list(fully_processed)
+
+# def listify_string_expression(string_expression: str):
+#     no_space: str = string_expression.replace(" ", "")
+#     listified: list[str] = no_space.split("+")
+
+#     return listified
+
+# def identify_all_shared_tokens(all_expression_strings: list[str]):
+#     flattened_strings: list[str]
+
+#     for expression_string in all_expression_strings:
+#         flattened_strings += listify_string_expression(expression_string)
 
 def load_csv(csv_path: str) -> list[dict[str, str]]:
     with open(csv_path) as file:
