c20c48954c125f5c32231a689ef6512a01d0ef62
MalinkyZubr <ahumanbeing189@gmail.com>
2024-08-02 20:53:53 -0700

    reorganized some python code

--- hardware/logic/scripts/tests/quine_profiler.py
+++
@@ -1,7 +0,0 @@
-import sys
-import cProfile
-sys.path.append("/home/malinkyzubr/Desktop/InterruptExpander/hardware/logic")
-
-from scripts.QuineMcluskey import *
-
-cProfile.run('pprint(minimize_dataset(load_csv(r"/home/malinkyzubr/Desktop/InterruptExpander/hardware/logic/scripts/TRUTH_TABLE2.csv")))')
