48e77e67e72359a149659ff75d697b7d098447b8
MalinkyZubr <ahumanbeing189@gmail.com>
2024-09-03 16:00:06 -0400

    fixed minor issue with interrupt handling in VirtualInterruptBoard.h

--- interface/include/VirtualInterruptBoard.hpp
+++ interface/include/VirtualInterruptBoard.hpp
@@ -18,16 +18,16 @@ inline void VI_Routine() {
     pinMode(VI_CS_PIN, OUTPUT);
     digitalWrite(VI_CS_PIN, LOW);
 
-    //const uint8_t send_address = 0b11111111;
     uint8_t interrupt_address = VIRead(0b11111111, SPISettings(4000000, MSBFIRST, SPI_MODE0)); // put SPISettings object here)
     VI_Manager.triggerVIInterrupt((InterruptAddress)interrupt_address);
 
+    digitalWrite(VI_CS_PIN, HIGH);
     pinMode(VI_CS_PIN, INPUT_PULLUP);
 
     sei();
 }
 
-#if TEST_BUILD == 1
+#if TEST_BUILD == 0
 ISR(VI_INTERRUPT_VECTOR) {
     VI_Routine();
 }
