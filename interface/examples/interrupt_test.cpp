3e6b9022565d017e17ba6429d1a494c4da3b93ab
MalinkyZubr <ahumanbeing189@gmail.com>
2024-09-03 16:13:58 -0400

    added serial writes to the test

--- interface/examples/interrupt_test.cpp
+++ interface/examples/interrupt_test.cpp
@@ -8,10 +8,12 @@ int execution_count = 0;
 
 void integrationTestISR() {
     execution_count++;
+    Serial.write("RECEIVED INTERRUPT 17");
 }
 
 int main() {
     SPI.begin();
+    Serial.begin();
     VI_Manager.attachVIInterrupt(VIRTUAL_INTERRUPT_17, integrationTestISR, 0);
 
     while(execution_count < 10) {
